9405abf2ad4dc3128ab6f99c8151a2f9bccdd767
geomatikaITERA <74612312+geomatikaITERA@users.noreply.github.com>
2020-11-18 18:55:50 +0700

    Add files via upload

---
+++ data/MENARATELPON_RBI_12.js
@@ -0,0 +1,8 @@
+var json_MENARATELPON_RBI_12 = {
+"type": "FeatureCollection",
+"name": "MENARATELPON_RBI_12",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAMOBJ": "Tower Telekomunikasi 0047 Diskominfo", "FCODE": null, "REMARK": "Menara Telepon", "SRS_ID": null, "LCODE": "HC0140", "METADATA": null }, "geometry": { "type": "Point", "coordinates": [ 105.293991452343732, -5.358804263006277 ] } }
+]
+}
